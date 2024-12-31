ba2992a849e46de7228750f163b0efff780acfbf
ramesh <109896095+ramesh2426@users.noreply.github.com>
2025-01-01 01:59:54 +0530

    Update CreateTable.java

--- CreateTable.java
+++ CreateTable.java
@@ -4,12 +4,12 @@ import java.sql.*;
 public class CreateTable{
     public static void main(String[] args){
 
-    String url = "jdbc:mysql://localhost:3306/JDBC";
+    String url = "jdbc:mysql://localhost:3306/test";
     String user = "root";
     String pass = "";
     String query = "create table if not exists students(id int primary key, name varchar(100), age int, grade varchar(20))";
-    try(){
-    Class.forName("com.mysql.cj.jdbc.");
+    try{
+    Class.forName("com.mysql.cj.jdbc.Driver");
     Connection con = DriverManager.getConnection(url,user,pass);
     Statement stmt = con.createStatement();
     stmt.execute(query);
